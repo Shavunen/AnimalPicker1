3847a2f304b7a193142a408a8d3bda7f826735dc
Shavunen <snhavunen@quinnipiac.edu>
2022-03-03 16:39:51 -0500

    cat and dog printlns

--- src/Animal1.java
+++ src/Animal1.java
@@ -1,4 +1,22 @@
 
-public class Animal1 {
+import java.util.Scanner;
 
+public class animal1 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		Scanner question = new Scanner(System.in);
+
+		System.out.println("Would you like to see a cat or a dog?");
+
+		String answer = question.nextLine();
+
+		if (answer.equalsIgnoreCase("cat")) {
+			System.out.println("“CAT ASCII ART HERE");
+
+		} else if (answer.equalsIgnoreCase("dog")) {
+			System.out.println("“DOG ASCII ART HERE");
+}
+	}
 }
